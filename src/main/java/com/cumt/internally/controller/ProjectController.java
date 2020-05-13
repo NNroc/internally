7429630bf5c93f757a2a155a8141af171cfe1f38
NNShadow <1581333251@qq.com>
2020-05-13 18:28:49 +0800

    controller init

---
+++ src/main/java/com/cumt/internally/controller/ProjectController.java
@@ -0,0 +1,11 @@
+package com.cumt.internally.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author NNroc
+ * @date 2020/5/13 18:28
+ */
+@RestController
+public class ProjectController {
+}
---
+++ src/main/java/com/cumt/internally/controller/RiskController.java
@@ -0,0 +1,11 @@
+package com.cumt.internally.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author NNroc
+ * @date 2020/5/13 18:02
+ */
+@RestController
+public class RiskController {
+}
