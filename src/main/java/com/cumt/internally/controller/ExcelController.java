4cc5fc1f82f2a039b7eed3cd065fe4565dff7dca
NNShadow <1581333251@qq.com>
2020-05-13 16:28:05 +0800

    import excel config

---
+++ src/main/java/com/cumt/internally/controller/ExcelController.java
@@ -0,0 +1,11 @@
+package com.cumt.internally.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author NNroc
+ * @date 2020/5/13 16:18
+ */
+@RestController
+public class ExcelController {
+}
