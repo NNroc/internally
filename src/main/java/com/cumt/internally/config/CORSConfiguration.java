f9808c9c4bb89cc746227c03bb87f81e3f4ec31d
NNShadow <1581333251@qq.com>
2020-05-20 12:20:53 +0800

    finish cors

---
+++ src/main/java/com/cumt/internally/config/CORSConfiguration.java
@@ -0,0 +1,27 @@
+package com.cumt.internally.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author NNroc
+ * @date 2020/5/20 12:20
+ */
+@Configuration
+public class CORSConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
