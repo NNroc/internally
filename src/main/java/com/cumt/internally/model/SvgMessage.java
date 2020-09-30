f3b102b7e3c8358ccdd5150794123941c83be889
NNShadow <1581333251@qq.com>
2020-09-30 13:51:36 +0800

    change pictures sort through discretization

--- src/main/java/com/cumt/internally/model/SvgMessage.java
+++ src/main/java/com/cumt/internally/model/SvgMessage.java
@@ -26,9 +26,40 @@ public class SvgMessage implements Common, Comparable<SvgMessage> {
         this.SVGSrc = SVGSrc;
     }
 
+    /**
+     * 将字符串转化为自定义的离散值，以便排序
+     *
+     * @param str 传入串
+     * @return
+     */
+    public double deal(String str) {
+        String use = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '.' || (str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+                use += str.charAt(i);
+            } else {
+                break;
+            }
+        }
+        double val = 0;
+        String use2[] = use.split("\\.");
+        for (int i = 0; i < use2.length; i++) {
+            val = val + Double.parseDouble(use2[i]) * Math.pow(101, 20 - i);
+        }
+        return val;
+    }
+
     @Override
     public int compareTo(SvgMessage svgMessage) {
-        return this.title.compareTo(svgMessage.title);
+        String str1 = this.title;
+        String str2 = svgMessage.title;
+        double num1 = deal(str1);
+        double num2 = deal(str2);
+        if (num1 > num2) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 
     @Override
